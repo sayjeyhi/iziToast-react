d16d6b331ac0e4523297843c454f9c65de4d9922
jafarRezaei <bomber.man87@yahoo.com>
2019-11-08 15:37:33 +0330

    Updates

---
+++ precache-manifest.d89c9e2d23e16a2b692c949b79292d03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d02cb77b783d74d302847390b9513f0",
+    "url": "/izitoast-react/index.html"
+  },
+  {
+    "revision": "1920e13f1397f4f62df3",
+    "url": "/izitoast-react/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "efe92914dece501d8364",
+    "url": "/izitoast-react/static/js/2.4e2a9b36.chunk.js"
+  },
+  {
+    "revision": "1920e13f1397f4f62df3",
+    "url": "/izitoast-react/static/js/main.11589a32.chunk.js"
+  },
+  {
+    "revision": "a4022d1c8add29bc4673",
+    "url": "/izitoast-react/static/js/runtime-main.26ffaa62.js"
+  }
+]);
