e48915b87260e1f966369c273c9ab540c256a5b9
jafarRezaei <bomber.man87@yahoo.com>
2019-11-15 14:40:03 +0330

    add styles [minified]

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
 import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy'
 
 import pkg from './package.json'
 
@@ -34,6 +35,11 @@ export default {
       rollupCommonJSResolveHack: true,
       clean: true
     }),
-    commonjs()
+    commonjs(),
+    copy({
+      targets: [
+        { src: 'src/iziToast.css', dest: 'dist' },
+      ]
+    })
   ]
 }
